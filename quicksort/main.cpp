7918b18b0bc101c0a9955cb9da589af14eaa07ea
breno <brenovambaster5@gmail.com>
2022-06-16 12:34:24 -0300

    readme

--- quicksort/main.cpp
+++ quicksort/main.cpp
@@ -5,11 +5,18 @@
 
 int main()
 {
+    /*
+    *********************************************************************************************
+    # COMPILAR APENAS UMA FUNÇÃO POR VEZ.
+    # PARA MOSTRAR O ESTADO DA LISTA A CADA INTERAÇÃO, MUDE O PARÂMETRO  "MSTR_ESTADO" PARA 1 NO ARQUIVO dependency.h
+    #
+    *********************************************************************************************
+    */
 
-    // lista_definida(); // teste  da avaliacao
+    lista_definida(); // teste  da avaliacao
 
     // random_list();
-    //   pior_caso(); // lista de tamanho 100;
-    //  lista_crescente();
-    lista_decrescente();
+    // pior_caso(); // lista de tamanho 100;
+    // lista_crescente();
+    // lista_decrescente();
 }
